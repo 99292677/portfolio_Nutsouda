9c5997c4804640a7c9692c6a645680075e78a153
99292677 <115576381+99292677@users.noreply.github.com>
2024-10-24 02:06:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 // ScrollReveal animations
-ScrollReveal().reveal('.hero', { delay: 200, distance: '50px', origin: 'top' });
 ScrollReveal().reveal('.card', { delay: 300, distance: '100px', origin: 'bottom' });
 ScrollReveal().reveal('.list-group-item', { delay: 400, distance: '50px', origin: 'left' });
